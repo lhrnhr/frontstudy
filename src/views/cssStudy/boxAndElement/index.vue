7ffb3680652a1a432991aedc0710f7b723e9f30d
lhrnhr <1225018666@qq.com>
2023-01-15 14:55:47 +0800

    2023.1.15 14:55

---
+++ src/views/cssStudy/boxAndElement/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="box1"></div>
+    <div class="box2"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BoxAndElement'
+}
+</script>
+
+<style>
+.box1{
+  width: 100px;
+  height: 200px;
+  background: red;
+}
+.box2{
+  width: 100px;
+  height: 200px;
+  background: green;
+}
+</style>
---
+++ src/views/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div>
+      <el-select v-model="value" placeholder="请选择" clearable="true">
+          <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value">
+          </el-option>
+        </el-select>
+    </div>
+      <BoxAndElement  v-if="value === 'BoxAndElement'"/>
+  </div>
+</template>
+
+<script>
+import BoxAndElement from '@/views/cssStudy/boxAndElement/index.vue'
+export default{
+  name: 'Notes',
+  components: {BoxAndElement},
+  data () {
+    return {
+      options: [{
+        value: 'BoxAndElement',
+        label: '盒模型和元素'
+      }],
+      value: ''
+    }
+  }
+}
+</script>
+
+<style>
+</style>
